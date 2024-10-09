c4578e8e315369f6f9397de274c21de0d52be586
Alb3rtoGitHub <ravallecillo@hotmail.com>
2024-10-09 15:46:03 -0300

    Agregar Empleado

--- src/empleados/ListadoEmpleados.js
+++ src/empleados/ListadoEmpleados.js
@@ -1,6 +1,26 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { NumericFormat } from 'react-number-format';
 
 export default function ListadoEmpleados() {
+
+    const urlBase = "http://localhost:8080/rh-app/empleados";
+    // **Uso de Hook variables de estado para monitorear cambios
+    const [empleados, setEmpleados] = useState([]);
+    // Cuando se carga la pagina se llama al Hook:
+    useEffect(() => {
+        cargarEmpleados();
+    }, []);
+
+    const cargarEmpleados = async () => {
+        // Petición al Back-End
+        const resultado = await axios.get(urlBase);
+        console.log("Resultado de cargar Empleados");
+        console.log(resultado.data);
+        // cargamos la info de los datos recibidos en resultado al arreglo de empleados**
+        setEmpleados(resultado.data);
+    }
+    
     return (
         <div className='container'>
             <div className='container text-center' style={{ margin: "30px" }}>
@@ -17,18 +37,22 @@ export default function ListadoEmpleados() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                    </tr>
+                    {
+                        // Iteramos el arreglo de empleados
+                        empleados.map((empleado, indice) => (
+                            <tr key={indice}>
+                                <th scope="row">{empleado.idEmpleado}</th>
+                                <td>{empleado.nombre}</td>
+                                <td>{empleado.departamento}</td>
+                                <td><NumericFormat value={empleado.sueldo}
+                                    displayType={'text'}
+                                    decimalSeparator=','
+                                    thousandSeparator='.' prefix={'u$s '}
+                                    decimalScale={2} fixedDecimalScale/>
+                                </td>
+                            </tr>
+                        ))
+                    }
                 </tbody>
             </table>
 
